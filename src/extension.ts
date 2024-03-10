4dbb6aa3fb632f2d834b800a44fef7a074ed248a
Nikhil Satiani <nikhil.satiani@gmail.com>
2024-03-11 00:05:25 +0500

    start socket server on startup and remove webview

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,70 @@
 import * as vscode from "vscode";
-import { ChatPanel } from "./panels/ChatPanel";
+import { WebSocket, WebSocketServer } from "ws";
+
+let wss: WebSocketServer;
+let wsClient: WebSocket | undefined;
 
 export function activate(context: vscode.ExtensionContext) {
-  const startSessionCommand = vscode.commands.registerCommand(
-    "copilotAtHome.startSession",
-    () => {
-      ChatPanel.render(context.extensionUri);
-    }
-  );
+  startWebSocketServer();
 
   const copyTextCommand = vscode.commands.registerCommand(
     "copilotAtHome.copyText",
-    () => {
-      const editor = vscode.window.activeTextEditor;
-      if (editor) {
-        const selectedText = editor.document.getText(editor.selection);
-        ChatPanel.postMessage({ command: "copyText", data: selectedText });
-      }
-    }
+    () => runCopyTextCommand({ type: "copy" })
   );
 
-  context.subscriptions.push(...[startSessionCommand, copyTextCommand]);
+  const copyTextAndSendCommand = vscode.commands.registerCommand(
+    "copilotAtHome.copyTextAndSend",
+    () => runCopyTextCommand({ type: "copyAndSend" })
+  );
+
+  context.subscriptions.push(...[copyTextCommand, copyTextAndSendCommand]);
 }
 
-export function deactivate() {}
+export function deactivate() {
+  wsClient?.close();
+  wss.close();
+}
+
+function startWebSocketServer() {
+  wss = new WebSocketServer({
+    port: 8765,
+  });
+
+  wss.on("connection", (client) => {
+    wsClient = client;
+
+    client.on("close", () => {
+      wsClient = undefined;
+    });
+  });
+}
+
+function runCopyTextCommand({ type }: Pick<Command, "type">) {
+  const content = getSelectedTextFromEditor();
+  if (content) {
+    runCommand({
+      type,
+      content,
+    });
+  }
+}
+
+function getSelectedTextFromEditor() {
+  const editor = vscode.window.activeTextEditor;
+  if (editor) {
+    return editor.document.getText(editor.selection);
+  }
+}
+
+interface Command {
+  type: "copy" | "copyAndSend";
+  content: string;
+}
+
+function runCommand(command: Command) {
+  if (!wsClient) {
+    return;
+  }
+
+  wsClient.send(JSON.stringify(command));
+}
